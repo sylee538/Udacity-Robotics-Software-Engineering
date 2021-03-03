d6fb5933eecf99567faa82ed565c60731a5355aa
SeungYeun Lee <sylee538@bu.edu>
2021-03-03 01:46:49 -0500

    comment edited
    drive_bot.cpp and process_image.cpp comment edited

--- Project2/catkin_ws/src/ball_chaser/src/drive_bot.cpp
+++ Project2/catkin_ws/src/ball_chaser/src/drive_bot.cpp
@@ -1,12 +1,12 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
-//TODO: Include the ball_chaser "DriveToTarget" header file
+//Include the ball_chaser "DriveToTarget" header file
 #include "ball_chaser/DriveToTarget.h"
 
 // ROS::Publisher motor commands;
 ros::Publisher motor_command_publisher;
 
-// TODO: Create a handle_drive_request callback function that executes whenever a drive_bot service is requested
+// Create a handle_drive_request callback function that executes whenever a drive_bot service is requested
 // This function should publish the requested linear x and angular velocities to the robot wheel joints
 // After publishing the requested velocities, a message feedback should be returned with the requested wheel velocities
 bool handle_drive_request(ball_chaser::DriveToTarget::Request& req, ball_chaser::DriveToTarget::Response& res) {
@@ -15,7 +15,7 @@ bool handle_drive_request(ball_chaser::DriveToTarget::Request& req, ball_chaser:
 
     // Create a motor_command object of type geometry_msgs::Twist
     geometry_msgs::Twist motor_command;
-  
+
     motor_command.linear.x = req.linear_x;
     motor_command.angular.z = req.angular_z;
     // Publish angles to drive the robot
@@ -42,12 +42,12 @@ int main(int argc, char** argv)
     // Inform ROS master that we will be publishing a message of type geometry_msgs::Twist on the robot actuation topic with a publishing queue size of 10
     motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 
-    // TODO: Define a drive /ball_chaser/command_robot service with a handle_drive_request callback function
+    // Define a drive /ball_chaser/command_robot service with a handle_drive_request callback function
     ros::ServiceServer service = n.advertiseService("/ball_chaser/command_robot", handle_drive_request);
     ROS_INFO("Ready to send drive commands");
 
 
-    // TODO: Handle ROS communication events
+    // Handle ROS communication events
     ros::spin();
 
     return 0;
--- Project2/catkin_ws/src/ball_chaser/src/process_image.cpp
+++ Project2/catkin_ws/src/ball_chaser/src/process_image.cpp
@@ -8,14 +8,14 @@ ros::ServiceClient client;
 // This function calls the command_robot service to drive the robot in the specified direction
 void drive_robot(float lin_x, float ang_z)
 {
-    // TODO: Request a service and pass the velocities to it to drive the robot
+    // Request a service and pass the velocities to it to drive the robot
   ROS_INFO_STREAM("Moving robot to designated place");
   ball_chaser::DriveToTarget srv;
-  
+
   srv.request.linear_x = lin_x;
   srv.request.angular_z = ang_z;
   ROS_INFO("Setting - linear_x: %1.2f, angular_z: %1.2f", (float)srv.request.linear_x, (float)srv.request.angular_z);
-  
+
   if(!client.call(srv))
     ROS_ERROR("Failed to call service command_robot");
 }
@@ -26,43 +26,29 @@ void process_image_callback(const sensor_msgs::Image img)
 
     int white_pixel = 255;
 
-//     bool turn_left = false;
-//     bool turn_right = false;
-//     bool move_forward = false;
-  
+
     int left_edge = img.step/3;
     int mid_edge = 2 * img.step/3;
     int right_edge = img.step;
-    
+
     // Loop through each pixel in the image and check if there's a bright white one
     for(int i = 0; i < img.height * img.step; i+=3) {
-      //ROS_INFO_STREAM("I'm INSIDE THE FOR LOOP");
       if (img.data[i] == white_pixel && img.data[i+1] == white_pixel && img.data[i+2] == white_pixel){
-    // Then, identify if this pixel falls in the left, mid, or right side of the image
-        //ROS_INFO_STREAM("DO YOU SEE ME?");
         int ball = i % img.step;
-        //std::cout << "FOUND IT!" << std::endl; //If the white ball is found
-        
+        // Then, identify if this pixel falls in the left, mid, or right side of the image
+        // Depending on the white ball position, call the drive_bot function and pass velocities to it
+        // Request a stop when there's no white ball seen by the camera
         if (ball >= 0 && ball <= left_edge){
-//           turn_left = true;
-//           turn_right = false;
-//           move_forward = false;
           ROS_INFO("Turning Left");
           drive_robot(0.1, -0.1);
           break;
         }
         else if (ball > left_edge && ball <= mid_edge){
-//           move_forward = true;
-//           turn_right = false;
-//           turn_left = false;
           ROS_INFO("Driving Forward");
           drive_robot(0.1, 0.0);
           break;
         }
         else if (ball > mid_edge && ball <= right_edge){
-//           turn_right = true;
-//           turn_left = false;
-//           move_forward = false;
           ROS_INFO("Turning Right");
           drive_robot(0.1,0.1);
           break;
@@ -73,24 +59,6 @@ void process_image_callback(const sensor_msgs::Image img)
         drive_robot(0.0, 0.0);
       }
     }
-    // Depending on the white ball position, call the drive_bot function and pass velocities to it
-    // Request a stop when there's no white ball seen by the camera
-//     if(turn_right){
-//       ROS_INFO("Turning Right");
-//       drive_robot(0.1,0.1);
-//     }
-//     else if(turn_left){
-//       ROS_INFO("Turning Left");
-//       drive_robot(0.1, -0.1);
-//     }
-//     else if(move_forward){
-//       ROS_INFO("Driving Forward");
-//       drive_robot(0.1, 0.0);
-//     }
-//     else if(!move_forward && !turn_left && !turn_right){
-//       ROS_INFO("Driving Stopped");
-//       drive_robot(0.0, 0.0);
-//     }
 }
 
 int main(int argc, char** argv)
